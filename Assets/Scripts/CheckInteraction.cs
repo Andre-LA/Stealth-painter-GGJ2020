89cc48d570000b594ef35f65d678063a691d15da
Paulo <paulo.zti@gmail.com>
2020-02-01 13:11:08 -0300

    First commit Paulo - Character movements and interactions

---
+++ Assets/Scripts/CheckInteraction.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheckInteraction : MonoBehaviour
+{
+    public GameObject alert;
+    public delegate void Interactive();
+    public static event Interactive onMonalisaStart, onMonalisaExit, onMonalisaReEnter;
+
+    private bool monalisaLoaded = false;
+    private bool nearInteractive = false;
+    void Update()
+    {
+        if(!monalisaLoaded && nearInteractive && Input.GetButtonDown("Jump"))
+        {
+            onMonalisaStart?.Invoke();
+            monalisaLoaded = true;
+        }
+        else if(monalisaLoaded && nearInteractive && Input.GetButtonDown("Jump"))
+        {
+            onMonalisaReEnter?.Invoke();
+        }
+        if(monalisaLoaded && Input.GetKeyDown(KeyCode.Escape))
+        {
+            onMonalisaExit?.Invoke();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Interactive"))
+        {
+            alert.gameObject.SetActive(true);
+            nearInteractive = true;
+            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Interactive"))
+        {
+            alert.gameObject.SetActive(false);
+            nearInteractive = false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+    public int startNode;
+    private int numberOfNodes;
+    private int currentNode;
+
+    public Transform[] moveSpots;
+    void Start()
+    {
+        waitTime = startWaitTime;
+        currentNode = startNode;
+        numberOfNodes = moveSpots.Length;
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[NextNode(currentNode)].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpots[NextNode(currentNode)].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                currentNode = NextNode(currentNode);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+
+    private int NextNode(int current)
+    {
+        int returning = current + 1;
+        if(returning == numberOfNodes)
+        {
+            returning = 0;
+        }
+        return returning;
+    }
+
+    
+}
