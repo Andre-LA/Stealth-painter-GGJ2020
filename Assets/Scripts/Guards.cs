774ea9f653f99df3dd1e2f6f0a2707204ecb165f
Tauã Amaro Pereira - 519119288 <519119288@animaedu.intranet>
2020-02-02 13:15:50 -0300

    Atualizacao fase 1 e guarda

--- Assets/Scripts/Guards.cs
+++ Assets/Scripts/Guards.cs
@@ -5,21 +5,16 @@ using UnityEngine;
 public class Guards : MonoBehaviour
 {
     private bool playerIsHiding = false;
-    private Animator Anim;
-    public float direcao = 0.5f;
-    public float axisX, axisY;
 
 
     void Start()
     {
         CheckInteraction.onPlayerHiding += HiddenPlayer;
-        Anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
-        verificaDirecao();
-        verificaAnimacao();
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -40,43 +35,5 @@ public class Guards : MonoBehaviour
         {
             playerIsHiding = true;
         }
-    }
-
-    public void verificaDirecao()
-    {
-        if (axisX > 0) { direcao = 0.5f; }                // valor do eixo positivo está indo para direita
-        else if (axisX < 0) { direcao = -0.5f; }          // valor do eixo negativo está indo para esquerda
-        transform.localScale = new Vector3(direcao, 0.5f, 0.5f);
-    }
-
-    public void verificaAnimacao()
-    {
-        int antes = Anim.GetInteger("situacao");
-
-        if (axisY < -0.05f)
-        {
-            Anim.SetInteger("situacao", 1);
-            return;
-        }
-        else
-        {
-            if (axisY > 0.05f)
-            {
-                Anim.SetInteger("situacao", 2);
-                return;
-            }
-        }
-
-        if (axisX > 0.05f || axisX < -0.05f)
-        {
-            Anim.SetInteger("situacao", 3);
-            return;
-
-        }
-
-        if (antes == 3)
-            Anim.SetInteger("situacao", 4);
-        else
-            Anim.SetInteger("situacao", 0);
     }
 }
